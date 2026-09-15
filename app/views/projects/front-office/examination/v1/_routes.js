a12bf8d6455095497a0032337dbcaa0d6993f92e
clairemcsolirius <claire.mcshane@solirius.com>
2026-09-15 16:17:23 +0100

    declaration page updated

--- app/views/projects/front-office/examination/v1/_routes.js
+++ app/views/projects/front-office/examination/v1/_routes.js
@@ -1,9 +1,24 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-router.post('/documents/local-plan-upload', function (req, res) {
-  req.session.data['local-plan-upload-complete'] = 'true'
-  req.session.data['local-plan-completed'] = 'true'
+router.get('*', function (req, res, next) {
+  // Add return to task list
+  res.locals['return'] = true
+
+  next()
+})
+
+router.post('/documents/examination-website-link', function (req, res) {
+  if (!req.session.data['examination-website-link']) {
+    req.session.data['examination-website-link'] = req.session.data['examination-library-link']
+  }
+  req.session.data['examination-website-link-complete'] = 'true'
+  res.redirect('../application-details')
+})
+
+// Fallback for any other document page (statement of changes, new documentation)
+router.post('/documents/:page', function (req, res) {
+  req.session.data[`${req.params.page}-complete`] = 'true'
   res.redirect('../application-details')
 })
 
